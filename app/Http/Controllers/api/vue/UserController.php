745bfcfc2d33309a5fd1e0277c95ec1304c42fe3
batch61 <ccsl61588@gmail.com>
2025-04-10 13:04:14 +0600

    Role Join

--- app/Http/Controllers/api/vue/UserController.php
+++ app/Http/Controllers/api/vue/UserController.php
@@ -12,7 +12,7 @@ class UserController extends Controller
  
     public function index(Request $request)
     {
-        $query = User::query();
+        $query = User::with('role');
 
         if ($request->search) {
             $query->where('name', 'like', "%{$request->search}%");
--- app/Models/User.php
+++ app/Models/User.php
@@ -55,4 +55,9 @@ class User extends Authenticatable implements JWTSubject
         'name'=>$this->name
       ];
     }
+
+    
+    function role(){
+      return $this->belongsTo(Role::class);
+    }
 }
